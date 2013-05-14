82d3880bd03e06fddfa93ce0f6816bf876a81602
Tomislav Matijevic <tomislav.m@netgen.hr>
2013-05-14 12:24:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+**Preview**: [ngResponsiveTables](http://jsfiddle.net/HyxAP/1/) - Resize the screen to see how it works :)
+
+**Warning**: This jquery plugin requires CSS3 in order to work properly!*
+
+ngResponsiveTables
+==========================
+
+This jQuery plugin converts regular table view into user friendly table for mobile devices.
+
+## Usage ##
+
+Default options:
+
+```javascript
+$(function(){
+  ngResponsiveTables.init({
+    tableSelector: '.ng-table',
+    smallPaddingMax: 13,
+  	mediumPaddingMax: 18,
+	largePaddingMax: 30
+  });
+});
+```
+
+## Options ##
+
+* __tableSelector__ - Table to use responsive styles on. You can use any element selector.
+* __smallPaddingMax__ - Set the max number of characters inside current table cell, and add class 'small-padding' on current td (if characters length is smaller than this number)
+* __mediumPaddingMax__ - Set the max number of characters inside current table cell, and add class 'medium-padding' on current td (if characters length is smaller than this number)
+* __largePaddingMax__ - Set the max number of characters inside current table cell, and add class 'large-padding' on current td (if characters length is smaller than this number)
+
+## WARNING ##
+All paddings must be declared in __ng_responsive_tables.css__ CSS file!
+
+## Idea ##
+Idea for this project came from [CSS Tricks](http://css-tricks.com/examples/ResponsiveTables/responsive.php)
+
+## Author ##
+
+Tomislav Matijević
+
---
+++ ng_responsive_tables.js
@@ -0,0 +1,61 @@
+/**
+* NG Responsive Tables v1.0
+* https://bitbucket.org/tmatijevic/ng_responsive_tables
+* Inspiration: http://css-tricks.com/examples/ResponsiveTables/responsive.php
+* Author: Tomislav Matijević
+* List of functions:
+*	- targetTable: Searches for each table row , find td and take its current index.
+*      Apply to that index same index of table head or td in first table row ( in case there are no table header applied )
+*	- checkForTableHead: If there is no table head defined, use td in first table row as table head (prevention mode)
+* Config:
+* - Adjust paddings
+* - On each td there is class named "tdno-[index]", so you can modify each td if you need custom padding
+*/
+
+(function(){
+
+	var defaults = {
+		tableSelector: '.ng-table',
+		bla: '',
+		smallPaddingMax: 0,
+		mediumPaddingMax: 0,
+		largePaddingMax: 0
+	},
+	ngResponsiveTables = {
+		opt: '',
+		dataContent: '',
+		globalWidth: 0,
+		init: function(params){
+			this.opt = $.extend( defaults, params );
+			ngResponsiveTables.targetTable();
+		},
+		targetTable: function(){
+			var that = this;
+			$(this.opt.tableSelector).find('tr').each(function(){
+				var $that = $(this);
+				$(this).find('td').each(function(i, v){
+					that.checkForTableHead( $(this), i );
+					$(this).addClass('tdno' + i);
+				});
+			});
+		},
+		checkForTableHead: function(element, index){
+			if( $(this.opt.tableSelector).find('th').length ){
+				this.dataContent = $(this.opt.tableSelector).find('th')[index].textContent;
+			}else{
+				this.dataContent = $(this.opt.tableSelector).find('tr:first td')[index].textContent;
+			}
+			// This padding is for large texts inside header of table
+			// Use small, medium and large paddingMax values from defaults to set-up offsets for each class
+			if( this.opt.smallPaddingMax > this.dataContent.length ){
+				element.addClass('small-padding');
+			}else if( this.opt.mediumPaddingMax > this.dataContent.length ){
+				element.addClass('medium-padding');
+			}else{
+				element.addClass('large-padding');
+			}
+			element.attr('data-content', this.dataContent);
+		}
+	};
+	window.ngResponsiveTables = ngResponsiveTables;
+})();
