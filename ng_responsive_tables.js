b8ce29c14ae16dde456d78b384c63bbf9656e99a
Tomislav Matijevic <tomislav.m@netgen.hr>
2013-05-17 13:35:52 +0200

    converting script to fully jquery plugin

--- ng_responsive_tables.js
+++ ng_responsive_tables.js
@@ -8,28 +8,28 @@
 *	- checkForTableHead: If there is no table head defined, use td in first table row as table head (prevention mode)
 * Config:
 * - Adjust paddings
-* - On each td there is class named "tdno-[index]", so you can modify each td if you need custom padding
+* - On each td there is class named "tdno[index]", so you can modify each td if you need custom padding
 */
 
-(function(){
-
-	var defaults = {
-		tableSelector: '.ng-table',
-		smallPaddingCharNo: 0,
-		mediumPaddingCharNo: 0,
-		largePaddingCharNo: 0
-	},
-	ngResponsiveTables = {
-		opt: '',
-		dataContent: '',
-		globalWidth: 0,
-		init: function(params){
-			this.opt = $.extend( defaults, params );
+;(function ( $ ) {
+	$.fn.ngResponsiveTables = function(options) {
+		var defaults = {
+		smallPaddingCharNo: 5,
+		mediumPaddingCharNo: 10,
+		largePaddingCharNo: 15
+		},
+		$selElement = this,
+		ngResponsiveTables = {
+			opt: '',
+			dataContent: '',
+			globalWidth: 0,
+		init: function(){
+			this.opt = $.extend( defaults, options );
 			ngResponsiveTables.targetTable();
 		},
 		targetTable: function(){
 			var that = this;
-			$(this.opt.tableSelector).find('tr').each(function(){
+			$selElement.find('tr').each(function(){
 				$(this).find('td').each(function(i, v){
 					that.checkForTableHead( $(this), i );
 					$(this).addClass('tdno' + i);
@@ -37,10 +37,10 @@
 			});
 		},
 		checkForTableHead: function(element, index){
-			if( $(this.opt.tableSelector).find('th').length ){
-				this.dataContent = $(this.opt.tableSelector).find('th')[index].textContent;
+			if( $selElement.find('th').length ){
+				this.dataContent = $selElement.find('th')[index].textContent;
 			}else{
-				this.dataContent = $(this.opt.tableSelector).find('tr:first td')[index].textContent;
+				this.dataContent = $selElement.find('tr:first td')[index].textContent;
 			}
 			// This padding is for large texts inside header of table
 			// Use small, medium and large paddingMax values from defaults to set-up offsets for each class
@@ -54,5 +54,11 @@
 			element.attr('data-content', this.dataContent);
 		}
 	};
-	window.ngResponsiveTables = ngResponsiveTables;
-})();
+
+	$(function(){
+		ngResponsiveTables.init();
+	});
+		return this;
+	};
+
+}( jQuery ));
